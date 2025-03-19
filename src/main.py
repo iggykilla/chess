081fa64b2363b763031a2f6513d58f03c94724d7
iggykilla <naciomarcos@gmail.com>
2025-03-19 17:17:23 +0000

    "Adding the code to display the window"

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
+import pygame
+import sys
+
+
+from const import *
+
 
 class Main:
 
     def __init__(self):
-        pass
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('Copy Chess')
+
 
     def mainLoop(self):
-        pass
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit(1)
+                    sys.exit(1)
+        
+            pygame.display.update() # last line of code
+
 
 main = Main()
 main.mainLoop()
