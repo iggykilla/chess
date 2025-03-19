7b30435c0fc981314554e3e7eadd544ccf316743
iggykilla <naciomarcos@gmail.com>
2025-03-19 14:43:06 +0000

    Refactored structure: moved files to src/, renamed chess.py to const.py

---
+++ src/const.py
@@ -0,0 +1,9 @@
+# Screen dimensions
+
+WIDTH = 800
+HEIGTH = 800
+
+# Board dimensions
+COLS = 8
+ROWS = 8
+SQSIZE =  WIDTH / COLS
---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+class Main:
+
+    def __init__(self):
+        pass
+
+    def mainLoop(self):
+        pass
+
+main = Main()
+main.mainLoop()
